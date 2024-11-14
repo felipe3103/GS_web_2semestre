9c4d68bd49c38ad0b1fe5f52fee1bf456503acde
Felipe <felipenafe@gmail.com>
2024-11-14 02:53:46 -0300

    img adicionada e pagina Error criada

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+function Error() {
+    return (
+      <div className="text-center">
+        <h1>404 - Página não encontrada</h1>
+        <p>Desculpe, essa página não existe.</p>
+      </div>
+    );
+  }
+  
+  export default Error;
